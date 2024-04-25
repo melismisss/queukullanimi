964241b8fccab36d5b32e33c4a997e4d8604eac5
melismisss <156009112+melismisss@users.noreply.github.com>
2024-04-25 18:52:33 +0300

    Add files via upload

---
+++ queu kullanımı/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace queu_kullanımı
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGetir = new System.Windows.Forms.Button();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.lblAd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(91, 117);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(127, 64);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle ";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGetir
+            // 
+            this.btnGetir.Location = new System.Drawing.Point(287, 117);
+            this.btnGetir.Name = "btnGetir";
+            this.btnGetir.Size = new System.Drawing.Size(127, 64);
+            this.btnGetir.TabIndex = 1;
+            this.btnGetir.Text = "Getir";
+            this.btnGetir.UseVisualStyleBackColor = true;
+            this.btnGetir.Click += new System.EventHandler(this.btnGetir_Click);
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(91, 225);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(133, 22);
+            this.txtAd.TabIndex = 2;
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(287, 230);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(44, 16);
+            this.lblAd.TabIndex = 3;
+            this.lblAd.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAd);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.btnGetir);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGetir;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label lblAd;
+    }
+}
+
